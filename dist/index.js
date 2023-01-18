6d437f5cc604bf0c898be8b41f4aab2ed3a38113
Bilal Z <bilalzubairi@hotmail.com>
2023-01-18 14:03:18 +0500

    install sfdx version 7.142

--- dist/index.js
+++ dist/index.js
@@ -13222,11 +13222,13 @@ const execCommand = __nccwpck_require__(5505);
 var fnInstallSFDX = function(){
     core.info('=== Download and install SFDX cli ===');
     //execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx-cli/channels/stable/sfdx-cli-v7.72.0-697e9faee2-linux-x64.tar.xz']);
-    execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz']);
+    // execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz']);
+    execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx/versions/7.172.0/1aa6bdf/sfdx-v7.142.0-1aa6bdf-linux-x64.tar.xz']);
     execCommand.run('mkdir', ['-p', 'sfdx']);
     //execCommand.run('tar', ['xJf', 'sfdx-cli-v7.72.0-697e9faee2-linux-x64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
     execCommand.run('tar', ['xJf', 'sfdx-linux-x64.tar.xz', '-C', 'sfdx', '--strip-components', '1']);
     // execCommand.run('export', ['PATH=./sfdx/bin:$PATH']);
+    execCommand.run('./sfdx/bin/sfdx', ['version'])
     core.info('=== SFDX cli installed ===');
 };
 
