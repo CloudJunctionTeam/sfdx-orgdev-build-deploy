459bd3fb7f51ac6317d800b9390b12baf909fa5e
Bilal Z <bilalzubairi@hotmail.com>
2023-01-18 13:02:53 +0500

    updated sfdx install steps

--- src/utils/install-dependencies.js
+++ src/utils/install-dependencies.js
@@ -2,13 +2,13 @@ const core = require('@actions/core')
 const execCommand = require('./exec-command.js');
 
 var fnInstallSFDX = function(){
-    core.info('=== Downloading and installing SFDX cli ===');
+    core.info('=== Download and install SFDX cli ===');
     //execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx-cli/channels/stable/sfdx-cli-v7.72.0-697e9faee2-linux-x64.tar.xz']);
     execCommand.run('wget', ['https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz']);
-    execCommand.run('mkdir', ['-p', 'sfdx-cli']);
+    execCommand.run('mkdir', ['~/sfdx']);
     //execCommand.run('tar', ['xJf', 'sfdx-cli-v7.72.0-697e9faee2-linux-x64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
-    execCommand.run('tar', ['xJf', 'sfdx-linux-x64.tar.xz', '-C', 'sfdx-cli', '--strip-components', '1']);
-    execCommand.run('./sfdx-cli/install', []);
+    execCommand.run('tar', ['xJf', 'sfdx-linux-x64.tar.xz', '-C', '~/sfdx', '--strip-components', '1']);
+    execCommand.run('export', ['PATH=~/sfdx/bin:$PATH']);
     core.info('=== SFDX cli installed ===');
 };
 
